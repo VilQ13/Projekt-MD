73869105b76a3ce37777906304265cccd61e722c
VilQ13 <rafal.adamaszek.k3+github@gmail.com>
2024-03-24 17:04:12 +0100

    asdf

--- Projekt MD.cpp
+++ Projekt MD.cpp
@@ -1,12 +1,70 @@
-﻿#include <iostream>
+﻿
 
-
+// C++ program to generate random numbers 
+#include <cstdlib> 
+#include <iostream> 
+#include <time.h> 
+#include <vector>
 
 using namespace std;
 
 
-
-int main()
+class Punkt
 {
-    cout << "Hello World!\n";
+public:
+    Punkt() 
+    {
+        
+    };
+
+    ~Punkt() {};
+
+    void  dodajWspolrzedne()
+    {
+        X = rand() % 20 + 1;
+        Y = rand() % 20 + 1;
+    }
+
+    void wyswietlWspolrzedne()
+    {
+        cout<< X<<" ";
+        cout << Y;
+    }
+
+
+    void dodajPolaczenie(int punkt)
+    {
+        Polaczenia.push_back(punkt);
+    }
+
+
+private:
+    int X, Y;
+    vector<int> Polaczenia;
+};
+
+int main() 
+{ 
+    int punktyMax = 7;
+
+    srand(time(0));
+
+    Punkt asdf[7];
+
+    for (int i = 0; i < punktyMax; i++)
+    {
+
+    asdf[i].dodajWspolrzedne();
+
+    cout << "punbkt " << i<<" ";
+    asdf[i].wyswietlWspolrzedne();
+
+    cout << endl;
+    }
+
+
+
+    return 0; 
+
 }
+
