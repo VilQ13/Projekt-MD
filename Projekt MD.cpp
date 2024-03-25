9983c27b29af864109db4fa02f3ab0fe6c5cc347
VilQ13 <rafal.adamaszek.k3+github@gmail.com>
2024-03-25 09:20:54 +0100

    asdf

--- Projekt MD.cpp
+++ Projekt MD.cpp
@@ -1,12 +1,53 @@
 ﻿#include <iostream>
-
-
+#include <cstdlib>
 
 using namespace std;
 
+int V = 5, 
+	E = 10;
+
+class Vertice
+{
+public:
+	int x, y;
+	Vertice();
+	~Vertice();
+
+private:
+	void create_point(int *x, int *y)
+	{
+
+	};
+};
+
+Vertice::Vertice()
+{
+	create_point(x,y);
+}
+
+Vertice::~Vertice()
+{
+}
 
 
 int main()
 {
-    cout << "Hello World!\n";
+	Vertice Vertice1;
+
+
+	int *vertices = new int[V];
+
+	for (int i = 0; i < V; i++)
+	{
+		vertices[i] = i + 1;
+	}
+
+
+	
+	
+	for (int i = 0; i < 15; i++)
+	{
+		int wybranyWierzch = rand() % V + 1;
+		cout << wybranyWierzch <<endl;
+	}
 }
